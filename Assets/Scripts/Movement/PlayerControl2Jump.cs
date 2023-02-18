7aec4c807749290a40b22c2eafadd6182247e239
Owen Mundy <omundy@gmail.com>
2023-02-18 16:15:28 -0500

    Add player, robot test, URP

---
+++ Assets/Scripts/Movement/PlayerControl2Jump.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *  How to jump like Super Mario in Unity 2D [Metroidvania Ep10] - Josh Codes
+ *  https://www.youtube.com/watch?v=39ia45WU5yc
+ *  https://github.com/JoshCodesStuff/Metroidvania/tree/episode-13
+ */
+
+[RequireComponent(typeof(Rigidbody2D))]
+//[RequireComponent(typeof(Animator))]
+public class PlayerControl2Jump : MonoBehaviour
+{
+    [Header("Jump Details")]
+    public float jumpForce;
+    public float jumpTime;
+    private float jumpTimeCounter;
+    private bool stoppedJumping;
+
+    [Header("Ground Details")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float radOCircle;
+    [SerializeField] private LayerMask whatIsGround;
+    public bool grounded;
+
+    [Header("Components")]
+    private Rigidbody2D rb;
+    //private Animator myAnimator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //myAnimator = GetComponent<Animator>();
+        jumpTimeCounter = jumpTime;
+    }
+
+    //myAnimator.SetBool("falling", true);
+    //myAnimator.SetBool("falling", false);
+
+    //myAnimator.SetTrigger("jump");
+    //myAnimator.ResetTrigger("jump");
+
+    private void Update()
+    {
+        //what it means to be grounded
+        grounded = Physics2D.OverlapCircle(groundCheck.position, radOCircle, whatIsGround);
+
+        if (grounded)
+        {
+            jumpTimeCounter = jumpTime;
+            //myAnimator.ResetTrigger("jump");
+            //myAnimator.SetBool("falling", false);
+        }
+
+        //if we press the jump button
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            //jump!!!
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            stoppedJumping = false;
+            //tell the animator to play jump anim
+            //myAnimator.SetTrigger("jump");
+        }
+
+        //if we hold the jump button
+        if (Input.GetButton("Jump") && !stoppedJumping && (jumpTimeCounter > 0))
+        {
+            //jump!!!
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpTimeCounter -= Time.deltaTime;
+            //myAnimator.SetTrigger("jump");
+        }
+
+        //if we release the jump button
+        if (Input.GetButtonUp("Jump"))
+        {
+            jumpTimeCounter = 0;
+            stoppedJumping = true;
+            //myAnimator.SetBool("falling", true);
+            //myAnimator.ResetTrigger("jump");
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            //myAnimator.SetBool("falling", true);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(groundCheck.position, radOCircle);
+    }
+
+    private void FixedUpdate()
+    {
+        HandleLayers();
+    }
+
+    private void HandleLayers()
+    {
+        if (!grounded)
+        {
+            //myAnimator.SetLayerWeight(1, 1);
+        }
+        else
+        {
+            //myAnimator.SetLayerWeight(1, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/PlayerControl2Movement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *  How to jump like Super Mario in Unity 2D [Metroidvania Ep10] - Josh Codes
+ *  https://www.youtube.com/watch?v=39ia45WU5yc
+ *  https://github.com/JoshCodesStuff/Metroidvania/tree/episode-13
+ */
+
+//[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerControl2Movement : MonoBehaviour
+{
+    //necessary for animations and physics
+    private Rigidbody2D rb2D;
+    //private Animator myAnimator;
+
+    private bool facingRight = true;
+
+    //variables to play with
+    public float speed = 2.0f;
+    public float horizMovement;//= 1[OR]-1[OR]0
+
+    private void Start()
+    {
+        //define the gameobjects found on the player
+        rb2D = GetComponent<Rigidbody2D>();
+        //myAnimator = GetComponent<Animator>();
+    }
+
+    //handles the input for physics
+    private void Update()
+    {
+        //check direction given by player
+        horizMovement = Input.GetAxisRaw("Horizontal");
+    }
+    //handles running the physics
+    private void FixedUpdate()
+    {
+        //move the character left and right
+        rb2D.velocity = new Vector2(horizMovement * speed, rb2D.velocity.y);
+        //myAnimator.SetFloat("speed", Mathf.Abs(horizMovement));
+        Flip(horizMovement);
+    }
+
+    //flipping function
+    private void Flip(float horizontal)
+    {
+        //if facing left && moving left
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+}
